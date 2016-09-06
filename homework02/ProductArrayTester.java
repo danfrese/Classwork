64529b720bdbfda3905d9cca29a6e385a3079493
Dan Frese <fresedan@gmail.com>
2016-09-06 16:35:37 -0500

    Added other homeworks and quizzes

---
+++ homework02/ProductArrayTester.java
@@ -0,0 +1,53 @@
+package homework02;
+
+import java.util.Scanner;
+import homework01.Product;
+
+/*
+ * @author: Dan Frese
+ */
+public class ProductArrayTester {
+
+	public static void main(String[] args) {
+
+		// Variable declarations
+		Scanner in = new Scanner(System.in);
+		int prodCount = 0;
+		int prodQuantity = 0;
+		String prodCode = "", prodDesc = "";
+		Double prodPrice = 0.00, totalSum = 0.00;
+
+		System.out.print("Please enter the number of products: ");
+		prodCount = in.nextInt();
+		Product[] cart = new Product[prodCount];// Initialize array
+
+		for (int i = 0; i < cart.length; i++) {
+			// Here there be "features"
+			System.out.println("Enter product code for product " + (i+1) + ": ");
+			prodCode = in.nextLine();
+			
+			System.out.println("Enter description for product " + (i+1) + ": ");
+			prodDesc = in.nextLine();
+
+			System.out.print("Enter quantity for product " + (i+1) + ": ");
+			prodQuantity = in.nextInt();
+
+			System.out.print("Enter price for product " + (i+1) + ": ");
+			prodPrice = in.nextDouble();
+
+			/*
+			 * Create a new Product object with populated values and add the new
+			 * object to the cart[] array.
+			 */
+			cart[i] = new Product(prodCode, prodDesc, prodPrice, prodQuantity);
+			
+			// Calculate total values
+			totalSum += cart[i].getTotalValue();
+		}
+		
+		in.close();
+		System.out.printf("Total value of items in cart is: $" + totalSum);
+
+	}
+
+}
