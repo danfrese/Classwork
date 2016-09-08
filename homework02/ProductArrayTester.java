1d1a18b5a4eabfda02c11dc51ce16bab51201f65
Dan Frese <fresedan@gmail.com>
2016-09-08 17:48:01 -0500

    Adjusted formatting on ProductArrayTester, answered the homework02 questions

--- homework02/ProductArrayTester.java
+++ homework02/ProductArrayTester.java
@@ -12,28 +12,26 @@ public class ProductArrayTester {
 
 		// Variable declarations
 		Scanner in = new Scanner(System.in);
-		int prodCount = 0;
-		int prodQuantity = 0;
+		int prodCount = 0, prodQuantity = 0;
+		double prodPrice = 0.00, totalSum = 0.00;
 		String prodCode = "", prodDesc = "";
-		Double prodPrice = 0.00, totalSum = 0.00;
 
 		System.out.print("Please enter the number of products: ");
 		prodCount = in.nextInt();
-		in.nextLine();
-		Product[] cart = new Product[prodCount];// Initialize array
-		
+		in.nextLine();// Clears the Scanner buffer
+		Product[] cart = new Product[prodCount];// Initialize cart[] array
+
 		for (int i = 0; i < cart.length; i++) {
-			// Here there be "features"			
-			System.out.println("Enter product code for product " + (i+1) + ": ");
+			System.out.println("Enter product code for product " + (i + 1) + ": ");
 			prodCode = in.nextLine();
-			
-			System.out.println("Enter description for product " + (i+1) + ": ");
+
+			System.out.println("Enter description for product " + (i + 1) + ": ");
 			prodDesc = in.nextLine();
 
-			System.out.print("Enter quantity for product " + (i+1) + ": ");
+			System.out.print("Enter quantity for product " + (i + 1) + ": ");
 			prodQuantity = in.nextInt();
 
-			System.out.print("Enter price for product " + (i+1) + ": ");
+			System.out.print("Enter price for product " + (i + 1) + ": ");
 			prodPrice = in.nextDouble();
 
 			/*
@@ -41,14 +39,12 @@ public class ProductArrayTester {
 			 * object to the cart[] array.
 			 */
 			cart[i] = new Product(prodCode, prodDesc, prodPrice, prodQuantity);
-			
+
 			// Calculate total values
 			totalSum += cart[i].getTotalValue();
 		}
-		
+
 		in.close();
 		System.out.printf("%s%.2f", "Total value of items in cart is: $", totalSum);
-
 	}
-
 }
