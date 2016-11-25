15a73efaeb797fab9e6d3e10d781602babb42873
Dan <fresedan@gmail.com>
2016-11-25 17:12:23 -0600

    Homework09 formatting and finished the necessary function in homework09
    I finished up with the code with a small bug of unknown origin. I've emailed Guy to see if the problem lies in my code or elsewhere.

--- homework09/InventoryParser.java
+++ homework09/InventoryParser.java
@@ -14,25 +14,21 @@ import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 /**
- *  An XML parser for product inventory.
+ * An XML parser for product inventory.
  */
-public class InventoryParser
-{
+public class InventoryParser {
 	private ArrayList<Product> products;
 	private DocumentBuilder builder;
 	private XPath path;
 
 	/**
-	 * Constructs a parser that can parse products in inventory,
-	 * and initializes the list of products in our inventory.
+	 * Constructs a parser that can parse products in inventory, and initializes
+	 * the list of products in our inventory.
 	 */
-	public InventoryParser() 
-		throws ParserConfigurationException
-	{
+	public InventoryParser() throws ParserConfigurationException {
 		products = new ArrayList<Product>();
-		DocumentBuilderFactory factory 
-			= DocumentBuilderFactory.newInstance();
-		//factory.setValidating(true);
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		// factory.setValidating(true);
 		factory.setIgnoringElementContentWhitespace(true);
 		builder = factory.newDocumentBuilder();
 		XPathFactory xpfactory = XPathFactory.newInstance();
@@ -41,40 +37,38 @@ public class InventoryParser
 
 	/**
 	 * Adds a product to inventory.
-	 * @param p the product to add
+	 * 
+	 * @param p
+	 *            the product to add
 	 */
-	public void addProduct(Product p)
-	{
+	public void addProduct(Product p) {
 		products.add(p);
 	}
 
 	/**
 	 * Finds a product with a given product code.
-	 * @param code the product code to find
-	 * @return the product with the given code, or null if there
-	 * is no such product
+	 * 
+	 * @param code
+	 *            the product code to find
+	 * @return the product with the given code, or null if there is no such
+	 *         product
 	 */
-	public Product find(String productCode)
-	{
-		for (Product p : products)
-		{
+	public Product find(String productCode) {
+		for (Product p : products) {
 			if (p.getCode().equals(productCode)) // Found a match
 				return p;
-		} 
+		}
 		// No match in the entire array list
-		throw new IllegalArgumentException("Product " + productCode +
-						   " was not found");
+		throw new IllegalArgumentException("Product " + productCode + " was not found");
 	}
-   
+
 	/**
 	 * Return a string that describes all the products in inventory.
 	 */
-	public String toString()
-	{
+	public String toString() {
 		double totalValue = 0.0;
 		StringBuffer sb = new StringBuffer();
-		for (Product p : products)
-		{
+		for (Product p : products) {
 			sb.append(p.toString());
 			sb.append('\n');
 			totalValue += p.getTotalValue();
@@ -82,39 +76,66 @@ public class InventoryParser
 		sb.append(String.format("Total value of inventory: %.2f\n", totalValue));
 		return sb.toString();
 	}
-	
+
 	/**
 	 * Parses an XML file containing the inventory of products.
-	 * @param fileName the name of the file
+	 * 
+	 * @param fileName
+	 *            the name of the file
 	 */
-	public void parse(String fileName) 
-		throws SAXException, IOException, XPathExpressionException
-	{
+	public void parse(String fileName) throws SAXException, IOException, XPathExpressionException {
 		File f = new File(fileName);
 		Document doc = builder.parse(f);
 
-		// Count the number of products, and then get
-		// the code, desc, price, and quantity for the product
-		// from the document, create a new Product object
-		// from the code, desc, price, and quantity, and add the
-		// product to the inventory using addProduct(). Refer
-		// back to Homework 4's readProducts() method where
-		// we created a Product object and added it to the inventory.
+		/*
+		 * Count the number of products, and then get the code, desc, price, and
+		 * quantity for the product from the document, create a new Product
+		 * object from the code, desc, price, and quantity, and add the product
+		 * to the inventory using addProduct().
+		 * 
+		 * Refer back to Homework 4's readProducts() method where we created a
+		 * Product object and added it to the inventory.
+		 */
 
-		...;
+		int prodCount = Integer.parseInt(path.evaluate("count(inventory/products)", doc));
+
+		for (int i = 1; i <= prodCount; i++) {
+			String code = path.evaluate("/inventory/products[" + i + "]/product/code", doc);
+			String description = path.evaluate("/inventory/products[" + i + "]/product/desc", doc);
+			int quantity = Integer.parseInt(path.evaluate("/inventory/products[" + i + "]/product/quantity", doc));
+			double price = Double.parseDouble(path.evaluate("/inventory/products[" + i + "]/product/price", doc));
+
+			Product pr = new Product(code, description, price, quantity);
+
+			addProduct(pr);
+		}
 
 		// Count the inventory transactions purchased. Then,
-		// for each purchased transaction, get the code and count, 
+		// for each purchased transaction, get the code and count,
 		// find the product by its code, and call the "purchased" method
 		// with the count.
-
-		...;
+		
+		int transPurchased = Integer.parseInt(path.evaluate("count(inventory/transactions/purchased)", doc));
+		
+		for (int i = 1; i <= transPurchased; i++) {
+			String code = path.evaluate("/inventory/transactions[" + i + "]/purchased/code", doc);
+			int count = Integer.parseInt(path.evaluate("/inventory/transactions[" + i + "]/purchased/count", doc));
+			
+			find(code).purchased(count);
+		}
 
 		// Count the inventory transactions sold. Then,
-		// for each sold transaction, get the code and count, 
+		// for each sold transaction, get the code and count,
 		// find the product by its code, and call the "sold" method
 		// with the sold quantity.
-
-		...;
+		
+		int transSold = Integer.parseInt(path.evaluate("count(inventory/transactions/sold)", doc));
+		
+		for (int i = 1; i <= transSold; i++) {
+			String code = path.evaluate("/inventory/transactions[" + i + "]/sold/code", doc);
+			int count = Integer.parseInt(path.evaluate("/inventory/transactions[" + i + "]/sold/count", doc));
+			
+			find(code).sold(count);
+		}
 	}
 }
--- homework09/Product.java
+++ homework09/Product.java
@@ -1,107 +1,109 @@
 package homework09;
 
 /**
- * A Product has a product code, description, price and 
- * a quantity that can be changed by purchases and sales.
+ * A Product has a product code, description, price and a quantity that can be
+ * changed by purchases and sales.
  */
-public class Product
-{
+public class Product {
 	private String code;
 	private String description;
 	private double price;
 	private int quantity;
 
 	/**
-	 * Constructs a product with a specific code,
-	 * description, price and quantity.
-	 * @param code - product lookup code
-	 * @param description - description of the product
-	 * @param price - cost of one product
-	 * @param quantity - number of products in inventory
+	 * Constructs a product with a specific code, description, price and
+	 * quantity.
+	 * 
+	 * @param code
+	 *            - product lookup code
+	 * @param description
+	 *            - description of the product
+	 * @param price
+	 *            - cost of one product
+	 * @param quantity
+	 *            - number of products in inventory
 	 */
-	public Product(String newCode, String newDescription,
-		       double newPrice, int newQuantity)
-	{
+	public Product(String newCode, String newDescription, double newPrice, int newQuantity) {
 		code = newCode;
 		description = newDescription;
 		price = newPrice;
 		quantity = newQuantity;
 	}
-	
+
 	/**
-	 * Increases the quantity of product when we've
-	 * purchased products to replenish our supply.
-	 * @param number the count of products purchased.
+	 * Increases the quantity of product when we've purchased products to
+	 * replenish our supply.
+	 * 
+	 * @param number
+	 *            the count of products purchased.
 	 */
-	public void purchased(int number)
-	{  
+	public void purchased(int number) {
 		int newQuantity = quantity + number;
 		quantity = newQuantity;
 	}
-	
+
 	/**
-	 * Decrease the quantity of product when we've
-	 * sold product to a customer.
-	 * @param number the count of products sold.
+	 * Decrease the quantity of product when we've sold product to a customer.
+	 * 
+	 * @param number
+	 *            the count of products sold.
 	 */
-	public void sold(int number)
-	{   
+	public void sold(int number) {
 		int newQuantity = quantity - number;
 		quantity = newQuantity;
 	}
-	
+
 	/**
 	 * Gets the quantity of this product.
+	 * 
 	 * @return the current quantity
 	 */
-	public double getQuantity()
-	{   
+	public double getQuantity() {
 		return quantity;
 	}
 
 	/**
 	 * Gets the code for this product.
+	 * 
 	 * @return the product code
 	 */
-	public String getCode()
-	{   
+	public String getCode() {
 		return code;
 	}
 
 	/**
 	 * Gets the code for this product.
+	 * 
 	 * @return the product code
 	 */
-	public String getDescription()
-	{   
+	public String getDescription() {
 		return description;
 	}
 
 	/**
 	 * Gets the product's price.
+	 * 
 	 * @return the product price
 	 */
-	public double getPrice()
-	{   
+	public double getPrice() {
 		return price;
 	}
 
 	/**
-	 * Get the total value in inventory of this product
-	 * (quantity times price).
+	 * Get the total value in inventory of this product (quantity times price).
 	 * return value
 	 */
-	public double getTotalValue()
-	{
+	public double getTotalValue() {
 		return quantity * price;
 	}
 
 	/**
 	 * Change the product's price by a given percent.
-	 * @param percent - percentage to change the product's price.
+	 * 
+	 * @param percent
+	 *            - percentage to change the product's price.
 	 */
-	public void adjustPrice(double percent)
-	{
+	public void adjustPrice(double percent) {
 		double change = price * (percent * 0.01);
 		// Round the change in price to two decimal points.
 		price = price + Math.round(change * 100.0) * 0.01;
@@ -109,11 +111,10 @@ public class Product
 
 	/**
 	 * Get a String that describes this Product.
+	 * 
 	 * @return info about this product
 	 */
-	public String toString()
-	{
-		return String.format("%s %s %d %.2f",
-				     code, description, quantity, price);
+	public String toString() {
+		return String.format("%s %s %d %.2f", code, description, quantity, price);
 	}
 }
